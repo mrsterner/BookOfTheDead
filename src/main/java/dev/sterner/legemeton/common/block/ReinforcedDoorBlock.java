112653b54bf02dce6327739bf3570361962a6055
mrsterner <simon@sterner.dev>
2022-11-19 03:53:04 +0100

    sync

--- src/main/java/dev/sterner/legemeton/common/block/ReinforcedDoorBlock.java
+++ src/main/java/dev/sterner/legemeton/common/block/ReinforcedDoorBlock.java
@@ -1,10 +1,9 @@
 package dev.sterner.legemeton.common.block;
 
 import dev.sterner.legemeton.common.registry.LegemetonObjects;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.DoorBlock;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.DoorHinge;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -15,7 +14,9 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.event.GameEvent;
 
 public class ReinforcedDoorBlock extends DoorBlock {
@@ -39,6 +40,22 @@ public class ReinforcedDoorBlock extends DoorBlock {
 			return ActionResult.PASS;
 		}
 		return super.onUse(state, world, pos, player, hand, hit);
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		DoubleBlockHalf doubleBlockHalf = state.get(HALF);
+		if (direction.getAxis() != Direction.Axis.Y || doubleBlockHalf == DoubleBlockHalf.LOWER != (direction == Direction.UP)) {
+			return doubleBlockHalf == DoubleBlockHalf.LOWER && direction == Direction.DOWN && !state.canPlaceAt(world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+		} else {
+			return neighborState.isOf(this) && neighborState.get(HALF) != doubleBlockHalf
+					? state.with(FACING, neighborState.get(FACING))
+					.with(OPEN, neighborState.get(OPEN))
+					.with(HINGE, neighborState.get(HINGE))
+					.with(POWERED, neighborState.get(POWERED))
+					.with(LOCKED, neighborState.get(LOCKED))
+					: Blocks.AIR.getDefaultState();
+		}
 	}
 
 	@Override
