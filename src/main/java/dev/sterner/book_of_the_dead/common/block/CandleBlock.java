6958aa33b8844feabf053d069bdcf172e2c7d7fc
mrsterner <simon@sterner.dev>
2023-04-09 20:33:33 +0200

    multiple candles

--- src/main/java/dev/sterner/book_of_the_dead/common/block/CandleBlock.java
+++ src/main/java/dev/sterner/book_of_the_dead/common/block/CandleBlock.java
@@ -5,6 +5,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
@@ -29,6 +32,9 @@ public class CandleBlock extends Block {
 	public static final IntProperty HEIGHT = IntProperty.of("height", 0, 4);
 	public static final float[] PARTICLE_HEIGHT = {0, 7 / 16f, 10 / 16f, 12 / 16f, 14 / 16f};
 	public static final float[] VOXEL_HEIGHT = {2, 5, 8, 10, 12};
+	public static final float[] VOXEL_WIDTH = {6, 5, 4};
+	//return Block.createCuboidShape(6.0, 0.0, 6.0, 10.0, VOXEL_HEIGHT[state.get(HEIGHT)], 10.0);
+	public static final IntProperty CANDLES = IntProperty.of("candles", 1, 3);
 
 	public CandleBlock(Settings settings) {
 		super(settings
@@ -41,7 +47,23 @@ public class CandleBlock extends Block {
 				.getDefaultState()
 				.with(Properties.LIT, false)
 				.with(HEIGHT, 4)
+				.with(CANDLES, 1)
 		);
+	}
+
+	@Override
+	public boolean canReplace(BlockState state, ItemPlacementContext context) {
+		return !context.shouldCancelInteraction() && context.getStack().getItem() == this.asItem() && state.get(CANDLES) < 3 || super.canReplace(state, context);
+	}
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		BlockState blockState = ctx.getWorld().getBlockState(ctx.getBlockPos());
+		if (blockState.isOf(this)) {
+			return blockState.cycle(CANDLES);
+		} else {
+			return super.getPlacementState(ctx);
+		}
 	}
 
 	@Override
@@ -74,12 +96,18 @@ public class CandleBlock extends Block {
 
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-		return Block.createCuboidShape(6.0, 0.0, 6.0, 10.0, VOXEL_HEIGHT[state.get(HEIGHT)], 10.0);
+		return Block.createCuboidShape(
+			VOXEL_WIDTH[state.get(CANDLES) - 1],
+			0.0,
+			VOXEL_WIDTH[state.get(CANDLES) - 1],
+			16 - VOXEL_WIDTH[state.get(CANDLES) - 1],
+			VOXEL_HEIGHT[state.get(HEIGHT)],
+			16 - VOXEL_WIDTH[state.get(CANDLES) - 1]);
 	}
 
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(Properties.LIT, HEIGHT);
+		builder.add(Properties.LIT, HEIGHT, CANDLES);
 		super.appendProperties(builder);
 	}
 
@@ -102,11 +130,28 @@ public class CandleBlock extends Block {
 	@Override
 	public void randomDisplayTick(BlockState state, World world, BlockPos pos, RandomGenerator random) {
 		if (state.get(Properties.LIT) && state.get(HEIGHT) > 0) {
-			double d = (double) pos.getX() + 0.5;
-			double e = (double) pos.getY() + PARTICLE_HEIGHT[state.get(HEIGHT)];
-			double f = (double) pos.getZ() + 0.5;
-			world.addParticle(ParticleTypes.SMOKE, d, e, f, 0.0, 0.0, 0.0);
-			world.addParticle(ParticleTypes.FLAME, d, e, f, 0.0, 0.0, 0.0);
+			switch (state.get(CANDLES)){
+				case 1 -> {
+					addParticles(0,0,0, state, world, pos);
+				}
+				case 2 -> {
+					addParticles(2 / 16D,- 2 / 16D,- 3 / 16D, state, world, pos);
+					addParticles(- 2 / 16D,0,3 / 16D, state, world, pos);
+				}
+				case 3 -> {
+					addParticles(- 3 / 16D,- 1 / 16D,- 4 / 16D, state, world, pos);
+					addParticles(4 / 16D,- 2 / 16D,- 1 / 16D, state, world, pos);
+					addParticles(- 2 / 16D,0,4 / 16D, state, world, pos);
+				}
+			}
 		}
+	}
+
+	public void addParticles(double x, double y, double z, BlockState state, World world, BlockPos pos){
+		double d = (double) pos.getX() + 0.5 + x;
+		double e = (double) pos.getY() + PARTICLE_HEIGHT[state.get(HEIGHT)] + y;
+		double f = (double) pos.getZ() + 0.5 + z;
+		world.addParticle(ParticleTypes.SMOKE, d, e, f, 0.0, 0.0, 0.0);
+		world.addParticle(ParticleTypes.FLAME, d, e, f, 0.0, 0.0, 0.0);
 	}
 }
--- src/main/java/dev/sterner/book_of_the_dead/common/block/CandleWallBlock.java
+++ src/main/java/dev/sterner/book_of_the_dead/common/block/CandleWallBlock.java
@@ -1,16 +1,25 @@
 package dev.sterner.book_of_the_dead.common.block;
 
+import dev.sterner.book_of_the_dead.common.registry.BotDObjects;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.random.RandomGenerator;
@@ -19,10 +28,11 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 
-public class CandleWallBlock extends CandleBlock {
+public class CandleWallBlock extends Block {
 	public static final IntProperty HEIGHT = IntProperty.of("height", 0, 4);
 	public static final float[] PARTICLE_HEIGHT = {0, 10 / 16f, 13 / 16f, 15 / 16f, 17 / 16f};
 	public static final float[] VOXEL_HEIGHT = {4, 8, 11, 13, 15};
@@ -36,6 +46,34 @@ public class CandleWallBlock extends CandleBlock {
 				.with(HEIGHT, 4)
 				.with(Properties.HORIZONTAL_FACING, Direction.NORTH)
 		);
+	}
+
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		ItemStack handStack = player.getMainHandStack();
+		if (hand == Hand.MAIN_HAND) {
+			if (handStack.isOf(BotDObjects.FAT) || handStack.isOf(Items.HONEYCOMB)) {
+				if (!state.get(Properties.LIT) && state.get(HEIGHT) < 4) {
+					world.setBlockState(pos, state.with(HEIGHT, state.get(HEIGHT) + 1));
+					if (!player.isCreative()) {
+						handStack.decrement(1);
+					}
+					return ActionResult.CONSUME;
+				}
+			} else if (handStack.isEmpty() && state.get(Properties.LIT)) {
+				world.setBlockState(pos, state.with(Properties.LIT, false));
+				world.playSound(null, pos, SoundEvents.ENTITY_GENERIC_EXTINGUISH_FIRE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+			} else if (state.get(HEIGHT) > 0 && (handStack.isOf(Items.FLINT_AND_STEEL) || handStack.isOf(Items.FIRE_CHARGE))) {
+				world.playSound(player, pos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+				world.setBlockState(pos, state.with(Properties.LIT, true), Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+				world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+				handStack.damage(1, player, p -> p.sendToolBreakStatus(hand));
+				return ActionResult.CONSUME;
+			}
+		}
+
+		return super.onUse(state, world, pos, player, hand, hit);
 	}
 
 	@Override
@@ -79,8 +117,7 @@ public class CandleWallBlock extends CandleBlock {
 
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(Properties.HORIZONTAL_FACING);
-		super.appendProperties(builder);
+		builder.add(Properties.LIT, HEIGHT, Properties.HORIZONTAL_FACING);
 	}
 
 	@Override
