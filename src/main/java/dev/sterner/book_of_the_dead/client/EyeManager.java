2709f96ad3947e85ac92e2d0f97fb15cf8fbb9c3
mrsterner <simon@sterner.dev>
2023-04-12 17:41:13 +0200

    eyemanager starts to look good

--- src/main/java/dev/sterner/book_of_the_dead/client/EyeManager.java
+++ src/main/java/dev/sterner/book_of_the_dead/client/EyeManager.java
@@ -3,17 +3,17 @@ package dev.sterner.book_of_the_dead.client;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import dev.sterner.book_of_the_dead.common.util.Constants;
-import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.random.RandomGenerator;
 import org.joml.Matrix4f;
 
 
 
 public class EyeManager {
+	public static EyeManager manager = null;
 	private RandomGenerator random;
 	public static final Identifier EYES_UP = Constants.id("textures/gui/eyes/1.png");
 	public static final Identifier EYES_MIDDLE = Constants.id("textures/gui/eyes/2.png");
@@ -24,54 +24,164 @@ public class EyeManager {
 	public int width = 14;
 	public int height = 10;
 	public SpriteCoordinates textureCoord = new SpriteCoordinates(2,2);
+	public final int MAX_Y = 4;
+	public final int MAX_X = 4;
 
-	public boolean isBlinking = false;
-	public boolean blinkingUp = false;
-	public int blinkingTimer = 0;
-	public int blinkCooldown = 20 * 10;
-
-	public int lookDirectionCooldown = 100;
-	public int lookTimer = 0;
 	public EyeDirection eyeDirection = EyeDirection.NONE;
+	public int moveDelay = 20 * 5;
+	public int blinkDelay = 20 * 2;
+
+	public int horizontalTick = 0;
+	public int verticalTick = 0;
+	public int blinkTick = 0;
+
+	boolean lookBack = false;
+	boolean blinkUpp = false;
+	boolean shouldResetVertical = false;
 
 	public EyeManager(RandomGenerator randomGenerator){
 		this.random = randomGenerator;
 	}
 
+	public static void tickEye(MinecraftClient client) {
+		if(client.world != null){
+			if (manager == null) {
+				manager = new EyeManager(client.world.random);
+			} else {
+				manager.tick();
+			}
+		}
+	}
+
 	public void tick(){
-		if (isBlinking) {
-			handleBlinkingUV();
-		} else if (lookDirectionCooldown < 0) {
-			lookDirectionCooldown = 100;
-			handleLookUV();
+		boolean blink = blink();
+		if(blink){
+			blinkTick = 0;
+			blinkUpp = false;
 		}
-		lookDirectionCooldown--;
-		blinkCooldown--;
+
+		boolean bl = switch (eyeDirection){
+			case LEFT -> lookLeft();
+			case RIGHT -> lookRight();
+			case UP -> lookUp(shouldResetVertical);
+			case DOWN -> lookDown(shouldResetVertical);
+			default -> false;
+		};
+
+		if (bl) {
+			eyeDirection = EyeDirection.NONE;
+			moveDelay = 20 * 5;
+			lookBack = false;
+		}
 	}
 
-	private void handleLookUV() {
-		lookTimer++;
-		switch (eyeDirection){
-			case UP -> {}
-			case DOWN -> {}
-			case LEFT -> {}
-			case RIGHT -> {}
-			default -> lookTimer = 0;
+	private boolean blink(){
+		boolean bl = false;
+		blinkTick++;
+		if(blinkTick > blinkDelay){
+			if(blinkUpp){
+				textureCoord = traverseUp(textureCoord);
+				if(textureCoord.y == 0){
+					bl = true;
+				}
+			}else{
+				textureCoord = traverseDown(textureCoord);
+				if(textureCoord.y == MAX_Y){
+					blinkUpp = true;
+				}
+			}
 		}
+
+
+		return bl;
 	}
 
-	private void handleBlinkingUV() {
-		if(!blinkingUp){
-			blinkingTimer++;
-			textureCoord = traverseUp(textureCoord);
-		}else{
-			textureCoord = traverseDown(textureCoord);
+	private boolean lookDown(boolean reset) {
+		boolean bl = false;
+		verticalTick++;
+		if(verticalTick >= moveDelay){
+			verticalTick = 0;
+			if(reset){
+				texture = EYES_MIDDLE;
+				bl = true;
+			}else{
+				texture = EYES_DOWN;
+			}
 		}
-		if(blinkingTimer > 20 * 3){
-			blinkingTimer = 0;
-			blinkingUp = true;
-		}
+
+		return bl;
 	}
+
+	private boolean lookUp(boolean reset) {
+		boolean bl = false;
+		verticalTick++;
+		if(verticalTick >= moveDelay){
+			verticalTick = 0;
+			if(reset){
+				texture = EYES_MIDDLE;
+				bl = true;
+			}else{
+				texture = EYES_UP;
+			}
+		}
+
+		return bl;
+	}
+
+	public boolean lookLeft(){
+		boolean bl = false;
+		horizontalTick++;
+		if(horizontalTick >= moveDelay){
+			horizontalTick = 0;
+			if(lookBack){
+				textureCoord = traverseRight(textureCoord);
+				if(textureCoord.x == 2){
+					bl = true;
+				}
+			}else{
+				textureCoord = traverseLeft(textureCoord);
+			}
+			if(textureCoord.x == 1 && random.nextFloat() < 0.35f){
+				lookBack = true;
+			} else if (textureCoord.x == 0) {
+				lookBack = true;
+			}
+			// Add a random delay offset to moveDelay
+			moveDelay += (int)(Math.random() * 10) - 5; // Offset by -5 to +5 ticks
+			if(moveDelay < 1){
+				moveDelay = 1;
+			}
+		}
+		return bl;
+	}
+
+	public boolean lookRight(){
+		boolean bl = false;
+		horizontalTick++;
+		if(horizontalTick >= moveDelay){
+			horizontalTick = 0;
+			if(lookBack){
+				textureCoord = traverseLeft(textureCoord);
+				if(textureCoord.x == 2){
+					bl = true;
+				}
+			}else{
+				textureCoord = traverseRight(textureCoord);
+			}
+			if(textureCoord.x == 3 && random.nextFloat() < 0.35f){
+				lookBack = true;
+			} else if (textureCoord.x == MAX_X) {
+				lookBack = true;
+			}
+			// Add a random delay offset to moveDelay
+			moveDelay += (int)(Math.random() * 10) - 5; // Offset by -5 to +5 ticks
+			if(moveDelay < 1){
+				moveDelay = 1;
+			}
+		}
+		return bl;
+	}
+
 
 
 	private static void drawTexturedQuad(Identifier texture, MatrixStack matrixStack, int x, int y, int width, int height, float minU, float maxU, float minV, float maxV) {
--- src/main/java/dev/sterner/book_of_the_dead/client/event/BotDClientEvents.java
+++ src/main/java/dev/sterner/book_of_the_dead/client/event/BotDClientEvents.java
@@ -1,16 +1,18 @@
 package dev.sterner.book_of_the_dead.client.event;
 
+import dev.sterner.book_of_the_dead.client.EyeManager;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.util.math.MatrixStack;
-import org.joml.Vector2i;
 import org.quiltmc.qsl.lifecycle.api.client.event.ClientTickEvents;
 
 public interface BotDClientEvents {
 
 	static void init() {
 		ClientTickEvents.END.register(ClientTickHandler::clientTickEnd);
+		ClientTickEvents.END.register(EyeManager::tickEye);
 		HudRenderCallback.EVENT.register(BotDClientEvents::renderEye);
 	}
+
 
 	static void renderEye(MatrixStack matrixStack, float tickDelta) {
 
